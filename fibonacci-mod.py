c63c0d1658d3357327571f07cde225b32e3ab5f1
ignatovegors <60047566+ignatovegors@users.noreply.github.com>
2020-01-20 16:59:42 +0300

    Update fibonacci-mod.py

--- fibonacci-mod.py
+++ fibonacci-mod.py
@@ -1,29 +1,18 @@
 def piz_seq(m):
-    a = 1
-    b = 1
-
-    for i in range(2, 6 * m + 1):
-        c = a + b
-        a = b
-        b = c
-
-        if a % m == 0 and b % m == 1:
+    assert n >= 0
+    f0, f1 = 0, 1
+    for i in range(6 * m - 1):
+        f0, f1 = f1, f0 + f1
+        if f0 % m == 0 and f1 % m == 1:
             return i
 
 
 def fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        a = 0
-        b = 1
-        for i in range(2, n + 1):
-            c = a + b
-            a = b
-            b = c
-        return c
+    assert n >= 0
+    f0, f1 = 0, 1
+    for i in range(n - 1):
+        f0, f1 = f1, f0 + f1
+    return f1
 
 
 def fib_mod(n, m):
